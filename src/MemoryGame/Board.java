45de453bfa60632703f063c3d130083775e48ad4
PolekM <marek.pawel.polek@gmail.com>
2022-06-15 11:01:30 +0200

    Create Board with Button

--- src/MemoryGame/Board.java
+++ src/MemoryGame/Board.java
@@ -1,26 +1,48 @@
 package MemoryGame;
 
 import javax.swing.*;
-import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Board {
     private JFrame frame;
-    private JPanel panel;
-    Board(){
+    private List<JButton> cardList = new ArrayList<>();
+
+    Board() {
         createBoard();
+
     }
-    private void createBoard(){
+
+    private void createBoard() {
+        createListOfCard();
         frame = new JFrame();
-        panel = new JPanel();
-
-        panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
-        panel.setLayout(new GridLayout(0,1));
-
-        frame.add(panel,BorderLayout.CENTER);
+        frame.setSize(600, 600);
+        frame.setLayout(null);
+        cardList.forEach(card -> frame.add(card));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setTitle("Memory Game");
-
-        frame.pack();
         frame.setVisible(true);
+
+
     }
+
+    private void createListOfCard() {
+        int x = 0;
+        int col = 45;
+        int row = 35;
+        while (x < 16) {
+            if (x % 4 == 0 && x != 0) {
+                row += 130;
+                col = 45;
+            }
+            Card tempCard = new Card(col, row);
+            cardList.add(tempCard.getMemoryCard());
+
+            col += 130;
+            x++;
+        }
+    }
+
 }
+
+
--- src/MemoryGame/Card.java
+++ src/MemoryGame/Card.java
@@ -1,4 +1,45 @@
 package MemoryGame;
 
+import javax.swing.*;
+
 public class Card {
+
+    private final int widthCard;
+    private final int heightCard;
+    private final int posX;
+    private final int posY;
+    private JButton memoryCard = new JButton();
+
+    Card(int posX, int posY) {
+        this.widthCard = 100;
+        this.heightCard = 100;
+        this.posX = posX;
+        this.posY = posY;
+        creatMemoryCard();
+    }
+
+    public int getWidthCard() {
+        return widthCard;
+    }
+
+    public int getHeightCard() {
+        return heightCard;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public JButton getMemoryCard() {
+        return memoryCard;
+    }
+
+    private void creatMemoryCard() {
+        this.memoryCard.setBounds(this.posX, this.posY, this.widthCard, this.heightCard);
+    }
+
 }
