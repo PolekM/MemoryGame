34ecdc1f78f1ed987d7f2b158d3dca84dc6afdb3
PolekM <marek.pawel.polek@gmail.com>
2022-06-22 14:57:44 +0200

    Adding images to card

--- src/MemoryGame/Card.java
+++ src/MemoryGame/Card.java
@@ -1,6 +1,7 @@
 package MemoryGame;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Card {
 
@@ -8,13 +9,15 @@ public class Card {
     private final int heightCard;
     private final int posX;
     private final int posY;
-    private JButton memoryCard = new JButton();
+    private final String cardImage;
+    private JButton memoryCard;
 
-    Card(int posX, int posY) {
+    Card(int posX, int posY, String cardImage) {
         this.widthCard = 100;
         this.heightCard = 100;
         this.posX = posX;
         this.posY = posY;
+        this.cardImage = cardImage;
         creatMemoryCard();
     }
 
@@ -39,7 +42,10 @@ public class Card {
     }
 
     private void creatMemoryCard() {
+
+        memoryCard = new JButton(new ImageIcon("images/reverse.png"));
         this.memoryCard.setBounds(this.posX, this.posY, this.widthCard, this.heightCard);
+
 
     }
 
